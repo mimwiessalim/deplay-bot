bb1efe68c8befb8f28e043d42b64dd7719acbaaa
mimwiessalim <mimwiessalimsa@gmail.com>
2025-10-18 16:05:23 +0200

    main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from telegram import Update, Bot
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import asyncio
 
-# Obtener token y URL desde las variables de entorno de Render
-TOKEN = os.getenv("7709737711:AAHCN8hgp27p_LSw9rLqjQhw6LffGd0swME")
-BOT_URL = os.getenv("https://deploy-bot.onrender.com")
+# -----------------------------
+# PON TU TOKEN AQUÍ
+TOKEN = "7709737711:AAHCN8hgp27p_LSw9rLqjQhw6LffGd0swME"  # Ej: "1234567890:ABCdefGHIjklMNOpqrSTUvwxYZ"
+
+# PON LA URL DE TU APP EN RENDER
+BOT_URL = "https://deploy-bot.onrender.com"  # La URL pública que Render te da
+# -----------------------------
 
 # Crear la aplicación de Telegram
 telegram_app = ApplicationBuilder().token(TOKEN).build()
